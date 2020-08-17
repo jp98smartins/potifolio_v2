34df2e129d70c45ae260013d949f7fdfdacfe882
João Pedro Martins <jp98smartins@gmail.com>
2020-08-17 04:35:38 -0300

    📞 Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,17 +2,28 @@ import React from "react"
 
 const contact = () => {
   return (
-    <div>
-      contact
-      <div>
-        <a
-          href="mailto:joaopedromartins@id.uff.br"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Fale Comigo!
-        </a>
+    <div className="ctc-data">
+      <div className="ctc-header">
+        <div className="ctc-title">
+          <span>IV.</span>Entre em contato
+        </div>
+        <div className="ctc-divider"></div>
       </div>
+      <div className="ctc-first-line">Vamos Conversar!</div>
+      <div className="ctc-second-line">
+        <p>
+          Me mande uma mensagem que te ajudo a transformar suas ideias em
+          realidade!
+        </p>
+        <p>Prometo que respondo o mais rápido que eu puder!</p>
+      </div>
+      <a
+        href="mailto:joaopedromartins@id.uff.br"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <div className="ctc-call-to-action">Fale Comigo!</div>
+      </a>
     </div>
   )
 }
