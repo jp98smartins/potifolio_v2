d97a7fc5ab30a1459391723208064eccbd7d3254
João Pedro Martins <jp98smartins@gmail.com>
2020-08-17 05:58:50 -0300

    🙋🏼‍♂️ About Me

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,7 +1,38 @@
 import React from "react"
+import abt_image from "../assets/images/abt-image.jpg"
 
 const aboutme = () => {
-  return <div>aboutme</div>
+  return (
+    <div className="abt-data">
+      <div className="abt-header">
+        <div className="abt-title">
+          <span>I.</span>Sobre Mim
+        </div>
+        <div className="abt-divider"></div>
+      </div>
+      <div className="abt-body">
+        <div className="abt-img">
+          <div className="img-back i-1"></div>
+          <div className="img-back i-2"></div>
+          <div className="img-overlay">
+            <img src={abt_image} alt="Foto de Perfil do João Pedro Martins" />
+          </div>
+        </div>
+        <div className="abt-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi iste
+          pariatur voluptatem modi maiores dolor, molestias iusto nemo hic. Ab
+          at debitis consectetur nostrum quos iste, tempora quam provident
+          voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quasi iste pariatur voluptatem modi maiores dolor, molestias iusto
+          nemo hic. Ab at debitis consectetur nostrum quos iste, tempora quam
+          provident voluptate. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quasi iste pariatur voluptatem modi maiores dolor,
+          molestias iusto nemo hic. Ab at debitis consectetur nostrum quos iste,
+          tempora quam provident voluptate.
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default aboutme
