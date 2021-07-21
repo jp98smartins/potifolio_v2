dde8a017545137d8977412067ee0f1eeac2c989e
João Pedro Martins <joao.martins@primeup.com.br>
2021-07-21 17:20:58 -0300

    Experiencia

---
+++ src/components/ExperienceDescription.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons"
+
+export const ExperienceDescription = props => {
+    return (
+    <div className="xp-job-description">
+        <div className="xp-job-name">
+          {props.position}
+          <a href={props.site} rel="noreferrer" target="_blank">
+            @{props.name}
+          </a>
+        </div>
+        <div className="xp-job-date">{props.date}</div>
+        {props.description.map((task, index) => (
+          <div className="xp-job-tasks" key={index}>
+            <FontAwesomeIcon
+              icon={faAngleDoubleRight}
+              className="arrow-right-icon"
+            />
+            {task}
+          </div>
+        ))}
+      </div>
+    )
+  }
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,18 +1,57 @@
 import React, { Component } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons"
+import {
+  ExperienceDescription,
+} from "../components/ExperienceDescription"
 
-// Testando build
+
+const primeupTrainee = {
+  name: "PrimeUp",
+  site: "http://www.primeup.com.br/",
+  position: "Trainee",
+  date: "Jul - Presente",
+  description: [
+    "Encarregado de desenvolver testes automatizados de uma solução da Anbima.",
+    "Os testes são feitos em Java, usando o Cucumber, BDD e o Selenium para os testes com interface.",
+    "Realizando também testes automatizados via mensageria da IBM (MQExplorer), servidor FTP e consultando os resultados em banco de dados da Oracle.",
+  ],
+}
+
+const primeupEstagio = {
+  name: "PrimeUp",
+  site: "http://www.primeup.com.br/",
+  position: "Estagiário",
+  date: "Nov/2020 - Jun/2021",
+  description: [
+    "Encarregado de desenvolver testes automatizados de uma solução da Anbima.",
+    "Os testes são feitos em Java, usando o Cucumber, BDD e o Selenium para os testes com interface.",
+    "Realizando também testes automatizados via mensageria da IBM (MQExplorer), servidor FTP e consultando os resultados em banco de dados da Oracle.",
+  ],
+}
+
+const addLabs = {
+  name: "AddLabs",
+  site: "https://www.addlabs.uff.br/Novo_Site_ADDLabs/",
+  position: "Desenvolvedor Mobile Jr.",
+  date: "Mar - Ago/2021",
+  description: [
+    "Encarregado de desenvolver aplicativo em Flutter para ser o backoffice de um app já existente.",
+    "O Aplicativo é multiplataforma (Android/IOS/Web), já desenvolvido em Flutter 2.0.",
+    "O backoffice e o app existente se conectam na mesma api externa.",
+    "O deploy do backoffice para o web foi feito usando docker no Azure.",
+  ],
+}
+
 const duo3 = {
   name: "Duo3",
   site: "http://sigoerp.com.br/",
-  position: "Estagiário",
-  date: "Fev - Presente",
+  position: "Desenvolvedor Mobile Jr.",
+  date: "Fev - Nov/2020",
   description: [
-    "Projeto de extensão resultante de parceria entre a UFF e a Engie.",
-    "Encarregado da elaboração de um software para o controle de tráfego urbano.",
-    "O projeto todo foi realizado utilizando a metodologia ágil (Scrum) com sprints de 2 semanas.",
-    "O software foi produzido no Visual Studio utilizando C# e MySQL com objetivo de rodar no Windows.",
+    "Encarregado da criação de um aplicativo mobile muiltiplataforma (Android e IOS) para controle de diários de obra.",
+    "O projeto foi pensado para se tornar uma extensão de um dos produtos da empresa Sigo ERP, um aplicativo web de gestão na construção civil.",
+    "O aplicativo foi produzido utilizando o Flutter e como banco de dados Hive.",
   ],
 }
 
@@ -20,7 +59,7 @@ const uff = {
   name: "UFF",
   site: "http://www.uff.br/",
   position: "Estagiário",
-  date: "Out - Fev 2019",
+  date: "Out - Fev/2019",
   description: [
     "Projeto de extensão resultante de parceria entre a UFF e a Engie.",
     "Encarregado da elaboração de um software para o controle de tráfego urbano.",
@@ -31,14 +70,53 @@ const uff = {
 
 class experience extends Component {
   state = {
-    job: "duo3",
-    duo3: "xp-menu-opt active",
+    job: "primeupTrainee",
+    primeupTrainee: "xp-menu-opt active",
+    primeupEstagio: "xp-menu-opt",
+    addLabs: "xp-menu-opt",
+    duo3: "xp-menu-opt",
     uff: "xp-menu-opt",
+  }
+
+  togglePrimeupTrainee = () => {
+    this.setState({
+      job: "primeupTrainee",
+      primeupTrainee: "xp-menu-opt active",
+      primeupEstagio: "xp-menu-opt",
+      addLabs: "xp-menu-opt",
+      duo3: "xp-menu-opt",
+      uff: "xp-menu-opt",
+    })
+  }
+
+  togglePrimeupEstagio = () => {
+    this.setState({
+      job: "primeupEstagio",
+      primeupTrainee: "xp-menu-opt",
+      primeupEstagio: "xp-menu-opt active",
+      addLabs: "xp-menu-opt",
+      duo3: "xp-menu-opt",
+      uff: "xp-menu-opt",
+    })
+  }
+
+  toggleAddLabs = () => {
+    this.setState({
+      job: "addLabs",
+      primeupTrainee: "xp-menu-opt",
+      primeupEstagio: "xp-menu-opt",
+      addLabs: "xp-menu-opt active",
+      duo3: "xp-menu-opt",
+      uff: "xp-menu-opt",
+    })
   }
 
   toggleDuo3 = () => {
     this.setState({
       job: "duo3",
+      primeupTrainee: "xp-menu-opt",
+      primeupEstagio: "xp-menu-opt",
+      addLabs: "xp-menu-opt",
       duo3: "xp-menu-opt active",
       uff: "xp-menu-opt",
     })
@@ -47,9 +125,64 @@ class experience extends Component {
   toggleUff = () => {
     this.setState({
       job: "uff",
+      primeupTrainee: "xp-menu-opt",
+      primeupEstagio: "xp-menu-opt",
+      addLabs: "xp-menu-opt",
       duo3: "xp-menu-opt",
       uff: "xp-menu-opt active",
     })
+  }
+
+  getExpDescription() {
+    if (this.state.job === "primeupTrainee") {
+      return <ExperienceDescription 
+        position={primeupTrainee.position}
+        site={primeupTrainee.site}
+        name={primeupTrainee.name}
+        date={primeupTrainee.date}
+        description={primeupTrainee.description}
+      />
+    }
+
+    if (this.state.job === "primeupEstagio") {
+      return <ExperienceDescription 
+        position={primeupEstagio.position}
+        site={primeupEstagio.site}
+        name={primeupEstagio.name}
+        date={primeupEstagio.date}
+        description={primeupEstagio.description}
+      />
+    }
+
+    if (this.state.job === "addLabs") {
+      return <ExperienceDescription 
+        position={addLabs.position}
+        site={addLabs.site}
+        name={addLabs.name}
+        date={addLabs.date}
+        description={addLabs.description}
+      />
+    }
+
+    if (this.state.job === "duo3") {
+      return <ExperienceDescription 
+        position={duo3.position}
+        site={duo3.site}
+        name={duo3.name}
+        date={duo3.date}
+        description={duo3.description}
+      />
+    }
+
+    if (this.state.job === "uff") {
+      return <ExperienceDescription 
+        position={uff.position}
+        site={uff.site}
+        name={uff.name}
+        date={uff.date}
+        description={uff.description}
+      />
+    }
   }
 
   render() {
@@ -63,6 +196,15 @@ class experience extends Component {
         </div>
         <div className="xp-body">
           <div className="xp-job-menu">
+            <button className={this.state.primeupTrainee} onClick={this.togglePrimeupTrainee}>
+              {primeupTrainee.name}
+            </button>
+            <button className={this.state.primeupEstagio} onClick={this.togglePrimeupEstagio}>
+              {primeupEstagio.name}
+            </button>
+            <button className={this.state.addLabs} onClick={this.toggleAddLabs}>
+              {addLabs.name}
+            </button>
             <button className={this.state.duo3} onClick={this.toggleDuo3}>
               {duo3.name}
             </button>
@@ -70,45 +212,7 @@ class experience extends Component {
               {uff.name}
             </button>
           </div>
-          {this.state.job === "duo3" ? (
-            <div className="xp-job-description">
-              <div className="xp-job-name">
-                {duo3.position}
-                <a href={duo3.site} rel="noreferrer" target="_blank">
-                  @{duo3.name}
-                </a>
-              </div>
-              <div className="xp-job-date">{duo3.date}</div>
-              {duo3.description.map((task, index) => (
-                <div className="xp-job-tasks" key={index}>
-                  <FontAwesomeIcon
-                    icon={faAngleDoubleRight}
-                    className="arrow-right-icon"
-                  />
-                  {task}
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div className="xp-job-description">
-              <div className="xp-job-name">
-                {uff.position}
-                <a href={uff.site} rel="noreferrer" target="_blank">
-                  @{uff.name}
-                </a>
-              </div>
-              <div className="xp-job-date">{uff.date}</div>
-              {uff.description.map((task, index) => (
-                <div className="xp-job-tasks" key={index}>
-                  <FontAwesomeIcon
-                    icon={faAngleDoubleRight}
-                    className="arrow-right-icon"
-                  />
-                  {task}
-                </div>
-              ))}
-            </div>
-          )}
+          {getExpDescription()}
         </div>
       </div>
     )
